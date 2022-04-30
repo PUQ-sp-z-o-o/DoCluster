4d5aeca81878048928a8ae0e760553fd106a0bcb
ruslan <it@puq.pl>
2022-04-30 18:36:21 +0200

    Quorum_6

--- DoCluster.py
+++ DoCluster.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import time
+
 from src.DoCluster_class import DoClusterMng
 
 
 Cluster = DoClusterMng()
+time.sleep(1)
 Cluster.MngStartWeb()
+time.sleep(1)
 Cluster.ApiStartWeb()
 
 #Cluster.MngSchedulersStart()

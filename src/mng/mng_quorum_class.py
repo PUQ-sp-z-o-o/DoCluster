34c5bb63e0e439c7f7cd306e46009c357e17aac1
ruslan <it@puq.pl>
2022-05-02 13:02:27 +0200

    Cluster_4

--- src/mng/mng_quorum_class.py
+++ src/mng/mng_quorum_class.py
@@ -72,18 +72,20 @@ class quorum(mng):
         while i < len(config.quorum_status['nodes']):
             if config.quorum_status['nodes'][i]['status'] in ['OK', 'online']:
                 config.quorum_status['master'] = config.quorum_status['nodes'][i]['node']
+                # Для того чтоб изменить порядок мастеров ПЕРЕДЕЛАТЬ!!!!!!!!!!!!!!!!!!!!!!!!!!
+                if config.quorum_status['master'] != config.cluster_config['quorum']['nodes'][0] and \
+                        config.quorum_status['master'] == os.uname()[1]:
+                    if config.quorum_status['master'] in config.cluster_config['quorum']['nodes']:
+                        print(str(config.cluster_config['quorum']['nodes']))
+                        config.cluster_config['quorum']['nodes'].remove(config.quorum_status['master'])
+                        config.cluster_config['quorum']['nodes'].insert(0, config.quorum_status['master'])
+                        print(str(config.cluster_config['quorum']['nodes']))
+                        config.logger.name = 'QUORUM'
+                        config.logger.info('Set new Master ' + config.quorum_status['master'])
+                        SaveConfiguration()
                 break
             i = i + 1
-        #Для того чтоб изменить порядок мастеров ПЕРЕДЕЛАТЬ!!!!!!!!!!!!!!!!!!!!!!!!!!
-        if config.quorum_status['master'] != config.cluster_config['quorum']['nodes'][0] and config.quorum_status['master'] == os.uname()[1]:
-            if config.quorum_status['master'] in config.cluster_config['quorum']['nodes']:
-                print(str(config.cluster_config['quorum']['nodes']))
-                config.cluster_config['quorum']['nodes'].remove(config.quorum_status['master'])
-                config.cluster_config['quorum']['nodes'].insert(0, config.quorum_status['master'])
-                print(str(config.cluster_config['quorum']['nodes']))
-                config.logger.name = 'QUORUM'
-                config.logger.info('Set new Master ' + config.quorum_status['master'])
-                SaveConfiguration()
+
 
     def QuorumSyncConfig(self):
         i = 0
